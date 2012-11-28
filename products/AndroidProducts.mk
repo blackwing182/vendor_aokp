aad5b818bfd423e20bf9b5915945d7828da27cf6
LorD ClockaN <davor@losinj.com>
2012-11-29 00:08:30 +0100

    remove unused devices

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,8 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/endeavoru.mk \
-    $(LOCAL_DIR)/grouper.mk \
-    $(LOCAL_DIR)/maguro.mk \
-    $(LOCAL_DIR)/mako.mk \
-    $(LOCAL_DIR)/manta.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/toroplus.mk
+    $(LOCAL_DIR)/endeavoru.mk
